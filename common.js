fe64c3aa21aa5a917051f031a245c4f1a1d49778
bill <wcstrickland1@gmail.com>
2024-01-02 17:55:04 -0600

    created study game

--- common.js
+++ common.js
@@ -1,4 +1,13 @@
 
 function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
