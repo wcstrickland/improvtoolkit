566bec515798b547046b90ab6246659e70764354
bill <wcstrickland1@gmail.com>
2024-01-02 18:20:12 -0600

    fixing game

--- common.js
+++ common.js
@@ -1,7 +1,10 @@
 
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
+    function randomNumber(min, max) {
+        min = parseInt(min)
+        max = parseInt(max)
+        let rand =  Math.floor(Math.random() * ((max - min) + 1) + min);
+        return rand
+    }
 
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
